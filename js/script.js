269ea4201a05eb4293668a6ac07e9e88a3ef8eeb
Arvin <akhadisurya@gmail.com>
2016-12-01 13:17:22 -0800

    create ajax function

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $('btnGetWeather').click(function (){
+    var requestData = $('#txtCity').val() + ',' + $('txtCountry').val();
+    var resultElement = $('#resultDiv');
+
+
+    $.ajax({
+      url: 'http://api.openweathermap.org/data/2.5/weather',
+      method: 'get',
+      data: { q: requestData },
+      dataType: 'json',
+      success: function (data) {
+        resultElement.html('Weather: ' + data.weather[0].main + '<br/>' + 'Description: ' + data.weather[0].description);
+      }
+    });
+  });
+});
