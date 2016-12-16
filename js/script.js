769533539632f87e33aedcf52e4a4a2b8551c082
Arvin <akhadisurya@gmail.com>
2016-12-15 16:18:28 -0800

    Adding pages

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$('#resultDiv').hide()
+
+$(document).ready(function () {
+  $('#btnGetWeather').click(function (){
+    $('#resultDiv').show()
+    var requestData = $('#txtCity').val() + ',' + $('txtCountry').val();
+    var resultElement = $('#resultDiv');
+
+
+    $.ajax({
+      url: 'http://api.openweathermap.org/data/2.5/weather?&units=imperial&APPID=d3d30f5b27665490813d939ed9013fc0',
+      method: 'get',
+      data: { q: requestData },
+      dataType: 'json',
+      success: function (data) {
+        if (data.message != null) {
+          resultElement.html(data.message);
+        } else {
+          resultElement.html('Weather: ' + data.weather[0].main + '<br/>' + 'Description: ' + data.weather[0].description + '<br/>' + 'Temperature: ' +  data.main.temp + ' °F');
+          console.log(data.name)
+        }
+      }
+      // error: function (err){
+      //   alert(err);
+
+    });
+  });
+});
